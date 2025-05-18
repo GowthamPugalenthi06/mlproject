600172470829b77bbb2657fe61efc342d920351f
NexgenAI024 <gowthamp1614@email.com>
2025-05-18 14:24:12 +0530

    Notebook

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,23 @@
+# src/exception.py
 import sys
+from src.logger import logger
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
-    
-    return error_message 
-        
+    return f"Error in file [{file_name}], line [{exc_tb.tb_lineno}]: {str(error)}"
+
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail=error_detail)
-    
+        self.error_message = error_message_detail(error_message, error_detail)
+        logger.error(self.error_message)  # <- Log here!
+
     def __str__(self):
         return self.error_message
-        
+
+if __name__ =="__main__":
+    try:
+        a = 10 / 0
+    except Exception as e:
+        raise CustomException(e, sys)
